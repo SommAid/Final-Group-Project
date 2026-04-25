a2d6c6f4051d4d7268a65dc6b4cd98cfc9b0a6ed
Aidan Sommer <aidansommeriii@gmail.com>
2026-04-25 18:48:30 -0400

    Rolling back env var insurance

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    OIDC_ISSUER: process.env.OIDC_ISSUER,
+    OIDC_CLIENT_ID: process.env.OIDC_CLIENT_ID,
+    OIDC_CLIENT_SECRET: process.env.OIDC_CLIENT_SECRET,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
 };
 
 export default nextConfig;
